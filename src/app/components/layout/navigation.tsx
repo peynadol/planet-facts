7d4fce6c79e79499c6e95464988c587b6448c7eb
peynadol <peynadol@gmail.com>
2025-05-22 09:44:43 +0100

    add active

--- src/app/components/layout/navigation.tsx
+++ src/app/components/layout/navigation.tsx
@@ -1,16 +1,35 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 const Navigation = () => {
+  const pathname = usePathname();
+  const links = [
+    { name: "Mercury", href: "/planets/mercury" },
+    { name: "Venus", href: "/planets/venus" },
+    { name: "Earth", href: "/planets/earth" },
+    { name: "Mars", href: "/planets/mars" },
+    { name: "Jupiter", href: "/planets/jupiter" },
+    { name: "Saturn", href: "/planets/saturn" },
+    { name: "Uranus", href: "/planets/uranus" },
+    { name: "Neptune", href: "/planets/neptune" },
+  ];
+
   return (
-    <div className="font-spartan opacity-60 font-bold text-[13px] gap-[33px] flex items-center">
-      <Link href="/planets/mercury">Mercury</Link>
-      <Link href="/planets/venus">Venus</Link>
-      <Link href="/planets/earth">Earth</Link>
-      <Link href="/planets/mars">Mars</Link>
-      <Link href="/planets/jupiter">Jupiter</Link>
-      <Link href="/planets/saturn">Saturn</Link>
-      <Link href="/planets/uranus">Uranus</Link>
-      <Link href="/planets/neptune">Neptune</Link>
+    <div className="font-spartan  font-bold text-[13px] gap-[33px] flex items-center">
+      {links.map((link) => (
+        <Link
+          key={link.name}
+          href={link.href}
+          className={clsx(
+            "hover:underline",
+            pathname === link.href ? "text-white" : "text-[#ffffff]/50"
+          )}
+        >
+          {link.name}
+        </Link>
+      ))}
     </div>
   );
 };
