7c2e88346eb247253e3e4e08eeafced1e3d39722
peynadol <peynadol@gmail.com>
2025-05-22 22:23:42 +0100

    center nav items

--- src/app/components/layout/header.tsx
+++ src/app/components/layout/header.tsx
@@ -1,9 +1,9 @@
-import Navigation from "./navigation";
+import Navigation from './navigation';
 
 const Header = () => {
   return (
-    <header className="flex justify-between uppercase border-b-2 border-white/20 px-6 py-4">
-      <h2 className="text-[28px] tracking-tighter font-medium">The Planets</h2>
+    <header className='flex justify-between uppercase border-b-2 border-white/20 px-6 py-4 items-center'>
+      <h2 className='text-[28px] tracking-tighter font-medium'>The Planets</h2>
       <Navigation />
     </header>
   );
--- src/app/planets/[planet]/page.tsx
+++ src/app/planets/[planet]/page.tsx
@@ -1,8 +1,8 @@
-import { notFound } from "next/navigation";
-import data from "../../data/data.json";
-import PlanetCard from "../../components/planets/planet-card";
-import PlanetStats from "../../components/planets/planet-stats";
-import PlanetClient from "../../components/planets/planet-client";
+import { notFound } from 'next/navigation';
+import data from '../../data/data.json';
+import PlanetCard from '../../components/planets/planet-card';
+import PlanetStats from '../../components/planets/planet-stats';
+import PlanetClient from '../../components/planets/planet-client';
 
 interface PlanetPageParams {
   params: Promise<{ planet: string }>;
@@ -19,7 +19,7 @@ export default async function PlanetPage({ params }: PlanetPageParams) {
   }
 
   return (
-    <div className="h-[calc(100dvh-76px)] flex flex-col justify-between px-4 sm:px-6 md:px-12 lg:px-[165px] pt-12 md:pt-[104px] pb-8 md:pb-[56px]">
+    <div className='h-[calc(100dvh-76px)] flex flex-col justify-between px-4 sm:px-6 md:px-12 lg:px-[165px] pt-12 md:pt-[104px] pb-8 md:pb-[56px]'>
       <PlanetCard>
         <PlanetClient planet={planetData} planetName={planetName} />
         <PlanetStats
